46bee83783f63a21f4f42677802d82c15a5d257c
Pragmatically-dev <pragmatically.dev@gmail.com>
2025-06-04 14:27:44 -0300

    Preparando ejercicio 2

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -1,53 +1,102 @@
-	.equ SCREEN_WIDTH, 		640
-	.equ SCREEN_HEIGH, 		480
-	.equ BITS_PER_PIXEL,  	32
+.include "constants.inc"
+/* -----------------------------------------------------
+ | CONVENCION DE USO DE REGISTROS QUE USAMOS EN ESTE LAB |
+  ------------------------------------------------------
+    X20        : Utilizado para manipular el FB
+ -------------------------------------------------------
+    X0  - X7   : INPUT/OUTPUT para funciones(The PCS says that the first argument is passed in X0, 
+                 the second argument in X1, and so on up to X7. Any further arguments are passed on the stack.)
 
-	.equ GPIO_BASE,      0x3f200000
-	.equ GPIO_GPFSEL0,   0x00
-	.equ GPIO_GPLEV0,    0x34
+    x8         : Registro usado por qemu
+---------------------------------------------------------
+The PCS defines which registers can be corrupted, and which registers cannot be
+corrupted. If a register can be corrupted, then the called function can overwrite without needing to restore
+    
+    x9  - x12  : Registros de uso temporal, no se necesita guardarlos ni restaurarlos
+    
+    x13 - x15  : Registros para contadores en los ciclos 
+    
+    x16 - x17  : IP0,IP1 Intra-procedure corruptible registers (segun armv8 ISA doc)
 
-	.globl main
+    x19 - X28  : (CALLEE-SAVED Registers) En caso de usarse se deben guardar y restaurar (LDR/STR)(Registros NO-VOLATILES)
+    
+    X29 - X31  : | NO USAR |FP Frame pointer, LR Link register, SP stack pointer
 
+RECAP:
+    La funcion foo() puede usar los registros x0-x15 sin la necesidad de preservar sus valores en la pila, en cambio
+    si la funcion foo() usa los registros x19-x28 la funcion DEBE guardar los valores en la pila y restablecerlos antes
+    de retornar.
+
+
+--------------------------------------------------------
+    CONVENCION PARA LA DEFINICION DE FIGURAS
+--------------------------------------------------------
+    nombre_de_funcion:
+    x1 --> x
+    x2 --> y
+    x3 --> color
+    x4 --> resto de parametros necesarios
+    .
+    .
+    .
+    x7
+-------------------------------------------------------
+ */
+    
+
+.globl main
 main:
-	// x0 contiene la direccion base del framebuffer
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+main_init:
 
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+    //Procedimiento [draw_static_background] escrito en: [background.s]
+	BL draw_static_background
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+    ADR X8,car_6
+    MOV W1,#170
+    MOV W2,#230
+    BL move_shape   
+    BL render_shape
 
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
 
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
+    ADR X8,car_4
+    MOV W1,#178
+    MOV W2,#60
+    BL move_shape
+    BL render_shape
 
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
+    ADR X8,car_9
+    MOV X1,#92
+    BL scale_shape
+    MOV W1,#289
+    MOV W2,#210
+    BL move_shape
+    BL render_shape
 
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
 
-	//---------------------------------------------------------------
-	// Infinite Loop
+    ADR X8,car_5
+    MOV W1,#560
+    MOV W2,#110
+    BL move_shape
 
-InfLoop:
-	b InfLoop
+    MOV X1,#42
+    BL scale_shape
+    BL render_shape
+
+    ADR X8,plane_1
+    MOV W1,#103
+    MOV W2,#60
+    BL move_shape
+    BL render_shape
+
+
+
+
+inf_loop:
+
+     
+
+	b inf_loop
+
---
+++ ejercicio2/background.s
@@ -0,0 +1,86 @@
+.include "constants.inc"
+
+.global "draw_static_background"
+draw_static_background:
+    STP X29,X30,[SP,#-16]!
+    MOV X29,SP
+    
+    //background
+    MOV X0,X20
+    MOV x1, #0 // x
+    MOV x2, #0 // y
+    LDR x3, =0x52b589 //color
+    MOV x4, #SCREEN_HEIGHT // alto
+    MOV x5, #SCREEN_WIDTH // ancho
+    BL draw_rectangle
+
+    //PASTO 2
+    MOV X0,X20
+    MOV x1, #0 // x
+    MOV x2, #240 // y
+    LDR x3, =0x309E6F // //color
+    MOV x4, #165 // alto
+    MOV x5, #640 // ancho
+    BL draw_rectangle
+
+
+    //BANQUINA 1
+    MOV X0,X20
+    MOV X1, #282
+    MOV X2,#160
+    LDR X3, =0xf7caa2
+    MOV X4,#7
+    MOV X5,#80
+    BL draw_shoulder_1
+
+    //BANQUINA 2
+    MOV X0,X20
+    MOV X1, #250
+    MOV X2,#160
+    LDR X3, =0xf7caa2
+    MOV X4,#7
+    MOV X5,#80
+    BL draw_shoulder_2
+    
+    //RUTA
+    MOV X0,X20
+    MOV X1, #286
+    MOV X2,#150
+    LDR X3, =0x50748c
+    MOV X4,#4
+    MOV X5,#40
+    BL draw_route
+
+    //CIELO 
+    MOV X0,X20
+    MOV x1, #0 // x
+    MOV x2, #0 // y
+    LDR x3, =0x08b9ce //color
+    MOV x4, #168 // alto
+    MOV x5, #640 // ancho
+    BL draw_rectangle
+
+    //SOL
+    MOV X0,X20
+    MOV X1,#465
+    MOV X2,#20
+    LDR X3,=0xF5BF42
+    MOV X4,#70
+    BL draw_circle
+
+    //LINEAS DE RUTA
+    BL draw_route_lines
+    
+    //MONTAÑAS
+    BL draw_mountain 
+
+
+    //CARTELES
+    BL draw_signals
+
+    //NUBES
+    BL draw_clouds
+
+
+    LDP X29,X30,[SP],#16
+    RET
